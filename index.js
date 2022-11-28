a0d11277f38c7cd053bcb51274d4ff3367c56338
Mattazz <matthewazada101@gmail.com>
2022-11-28 22:33:54 +0800

    add logo rotation

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// $('h1').text('Hello!');
+
+let i = 1;
+let x = 0;
+
+function logorot(){
+    let rotation = 'rotate(' + x + 'deg)'
+    $('#logo-image').css('transform', rotation)
+    x++
+    if(x<1000){
+        setTimeout(logorot, 50);
+    }
+}
+logorot()
+
+
+// document.getElementById('logo-image').style.transform = 'rotate(45deg)'
